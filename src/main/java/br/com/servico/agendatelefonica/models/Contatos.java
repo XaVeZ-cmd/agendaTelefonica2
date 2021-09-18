261194945cd7302256fb2f04a3ecd2f0bbbfd766
Thiago Amorim <thiago.amorim_2@live.com>
2021-09-18 17:12:01 -0300

    Adicionando beck-end serviço de agenda de contatos

---
+++ src/main/java/br/com/servico/agendatelefonica/models/Contatos.java
@@ -0,0 +1,24 @@
+package br.com.servico.agendatelefonica.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Contatos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+}
