0e5b3c196f218ee431f2a8988cd1d43657b58fcd
Weslley <weslleyjonathas88@gmail.com>
2021-10-18 19:10:04 -0300

    .....

--- src/main/java/br/com/servico/agendatelefonica/service/EmailService.java
+++ src/main/java/br/com/servico/agendatelefonica/service/EmailService.java
@@ -6,10 +6,7 @@ import br.com.servico.agendatelefonica.models.entity.Email;
 import br.com.servico.agendatelefonica.repository.EmailRepository;
 import br.com.servico.agendatelefonica.utils.Messages;
 import br.com.servico.agendatelefonica.utils.exceptions.BusinessException;
-import br.com.servico.agendatelefonica.utils.exceptions.EmailException;
-import br.com.servico.agendatelefonica.utils.exceptions.NotFoundException;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -18,134 +15,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-
+    
     @Autowired
     private EmailRepository emailRepository;
 
     @Autowired
     private EmailMapper emailMapper;
 
-    // Salvar email no Banco de dados
     @Transactional
-    public EmailDTO save(EmailDTO emailDto) {
-        Optional<Email> optionalEmail = emailRepository.findByEmail(emailDto.getEmailDTO());
+    public EmailDTO save(EmailDTO emailDTO) {
+        Optional<Email> optionalEmail = emailRepository.findByEmail(emailDTO.getEmailDTO());
         if (optionalEmail.isPresent()) {
-            throw new EmailException(Messages.EMAIL_EXISTE);
+            throw new BusinessException(Messages.EMAIL_EXISTE);
+        }
+        Email email = emailMapper.toEntity(emailDTO);
+        emailRepository.save(email);
+        return emailMapper.toDTO(email);
+    }
+
+    @Transactional
+    public EmailDTO update(EmailDTO emailDTO) {
+        Optional<Email> optionalEmail = emailRepository.findByEmail(emailDTO.getEmailDTO());
+        if (optionalEmail.isPresent()) {
+            throw new BusinessException(Messages.EMAIL_NAO_EXISTE);
         }
 
-        Email email = emailMapper.toEntity(emailDto);
+        Email email = emailMapper.toEntity(emailDTO);
         emailRepository.save(email);
-        return emailMapper.toDto(email);
+        return emailMapper.toDTO(email);
     }
 
-    // Atualizar email por id
-    @Transactional
-    public EmailDTO update(EmailDTO emailDto) {
-        Optional<Email> optionalEmail = emailRepository.findByEmailUpdate(emailDto.getIdEmailDTO());
-        if (optionalEmail.isPresent()) {
-            throw new EmailException(Messages.EMAIL_EXISTE);
-        }
-
-        Email email = emailMapper.toEntity(emailDto);
-        emailRepository.save(email);
-        return emailMapper.toDto(email);
-    }
-
-    @Transactional
-    public EmailDTO delete(EmailDTO emailDTO) {
-        Optional<EmailDTO> emailDto = findByIdEmail(emailDTO.getIdEmailDTO());
-        return emailRepository.deleteById(emailDTO);
-
-    }
-
-    @Transactional
-    public Optional<EmailDTO> findByIdEmail(EmailDTO emailDto) {
-        return emailRepository.findByIdEmail(emailDto.getIdEmailDTO()).map(emailMapper::toDto);
-        Optional<Email> optionalEmail = emailRepository.findByEmailUpdate(emailDto.getIdEmailDTO());
-
-        if (optionalEmail.isPresent()) {
-            throw new EmailException(Messages.EMAIL_EXISTE);
-        }
-
-        Email email = emailMapper.toEntity(emailDto);
-        emailRepository.save(email);
-        return emailMapper.toDto(email);
-    }
-
-   private EmailDTO findByIdEmail(Long idEmail) {
-       return emailRepository.findByIdEmail(idEmail).map(emailMapper::toDto).orElseThrow(NotFoundException::new);
-
-    @Transactional(readOnly = true)
-    public List<EmailDTO> findAll() {
-        return emailMapper.toDto(emailRepository.findAll());
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     @Transactional
-//     public List<EmailDTO> findAll() {
-//         return emailMapper.toDTO(emailemailRepository.findAll());
-//     }
-
-//     @Transactional
-//    private EmailDTO findByIdEmail(Long idEmail) {
-//        return emailemailRepository.findByIdEmail(idEmail).map(emailMapper::toDto).orElseThrow(NotFoundException::new);
-
-    
-
-//     // Metodo para salvar
-//     @Transactional
-//     public EmailDTO save(EmailDTO emailDTO) {
-//         Optional<Email> optionalEmail = emailemailRepository.findByEmail(emailDTO.getEmailDTO());
-//         if (optionalEmail.isPresent()) {
-//             throw new BusinessException(Messagess.EMAIL_EXISTE);
-//         }
-//         Email email = emailMapper.toEntity(emailDTO);
-//         emailemailRepository.save(email);
-//         return emailMapper.toDTO(email);
-//     }
-
-//     // Metodo para atualizar
-//     @Transactional
-//     public EmailDTO update(EmailDTO emailDTO) {
-//         Optional<Email> optionalEmail = emailemailRepository.findByEmail(emailDTO.getEmailDTO());
-//         if (optionalEmail.isPresent()) {
-//             throw new BusinessException(Messagess.EMAIL_NAO_EXISTE);
-//         }
-
-//         Email email = emailMapper.toEntity(emailDTO);
-//         emailemailRepository.save(email);
-//         return emailMapper.toDTO(email);
-//     }
-
-//     // Metodo para deletar
-//     @Transactional
-//     public EmailDTO delete(EmailDTO emailDTO) {
-//         Optional<EmailDTO> optionalEmailDTO = findByIdEmail(emailDTO.getIdEmailDTO());
-//         return emailemailRepository.deleteById(emailDTO);
-
-//     }
 }
--- src/main/java/br/com/servico/agendatelefonica/utils/exceptions/EmailException.java
+++ src/main/java/br/com/servico/agendatelefonica/utils/exceptions/EmailException.java
@@ -1,10 +1,8 @@
 package br.com.servico.agendatelefonica.utils.exceptions;
 
-import br.com.servico.agendatelefonica.utils.Messages;
-
 public class EmailException extends RuntimeException{
-    public EmailException(){
-        super(Messages.EMAIL_NAO_EXISTE);
+    public EmailException(String message){
+        super(message);
     }
 
 }
